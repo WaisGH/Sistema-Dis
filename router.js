edf305ed2f8182a6eeb8cb808876af5f07056177
WaisGH <158162498+WaisGH@users.noreply.github.com>
2026-06-15 16:11:49 -0300

    Add files via upload

--- router.js
+++ router.js
@@ -5,24 +5,20 @@ import ingressoValidator from "./validator/ingressoValidator.js";
 import notificacaoValidator from "./validator/notificacaoValidator.js";
 
 const routes = new Router();
+
 // Rotas para Ingressos
 routes.post(
   "/ingressos",
   ingressoValidator.editAction,
   IngressoController.store,
 );
-routes.post("/ingressos", IngressoController.show);
+routes.get("/ingressos", IngressoController.show);
 routes.get("/ingressos/:id", IngressoController.index);
 routes.delete("/ingressos/:id", IngressoController.destroy);
 
 // Rotas para Notificações
 routes.get("/notificacoes", NotificacaoController.index);
 routes.get("/notificacoes/:id", NotificacaoController.show);
-
-routes.post(
-  "/notificacoes",
-  notificacaoValidator.editAction,
-  NotificacaoController.store,
-);
+routes.delete("/notificacoes/:id", NotificacaoController.destroy);
 
 export default routes;
